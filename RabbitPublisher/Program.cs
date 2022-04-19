a466652e381d7ea4da9220ba621825926d8ae7ea
Lucas Freitas <lucas@xsys.com.br>
2022-04-19 12:16:13 -0300

    Add project files.

---
+++ RabbitPublisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitPublisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "bomdia_1",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello, World in RabbitMQ!";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "bomdia_1",
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine($" [x] Enviada: {message}");
+            }
+        }
+    }
+}
